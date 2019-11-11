a21c42a1ccb76e4fd864faf64b67c6e90992ca28
carolineireri <carolnyamburaireri@gmail.com>
2019-11-11 14:27:52 +0300

    Replaced items div

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
-//constructor for pizza
-function Pizza(type, size, crust) {
-    this.type = type;
-    this.size = size;
-    this.crust = crust;
-    this.toppings = [];
-  }
-  //price and size
-  var sizePrice = {
-    small: 700,
-    medium: 900,
-    large: 1200
-  };
-  var toppingPrice = [
-    {
-        small: 50,
-        medium: 100,
-        large: 150
-     
-      }
-  ];
+// //constructor for pizza
+// function Pizza(type, size, crust) {
+//     this.type = type;
+//     this.size = size;
+//     this.crust = crust;
+//     this.toppings = [];
+//   }
+//   //price and size
+//   var sizePrice = {
+//     small: 700,
+//     medium: 900,
+//     large: 1200
+//   };
+
 //   var toppingPrice = [
 //     {
 //       bacon: {
@@ -38,18 +31,229 @@ function Pizza(type, size, crust) {
 //       }
 //     }
 //   ];
-   //location constructor
-function Location(name, estate) {
+//    //location constructor
+// function Location(name, estate) {
+//     this.name = name;
+//     this.estate = estate;
+//   }
+//   //crust price
+//   var crustPrice = {
+//     crispy: 200,
+//     stuffed: 200,
+//     gluten: 150
+//   };
+//   //function calculate prize according to size
+//   function sizeCalcPrice(size) {
+//     if (size === "small") {
+//       return sizePrice.small * 1;
+//     } else if (size === "medium") {
+//       return sizePrice.medium * 1;
+//     } else {
+//       return sizePrice.large * 1;
+//     }
+//   }
+//   //price according to crust
+//   function crustCalcPrice(crust) {
+//     if (crust === "crispy") {
+//       return crustPrice.crispy * 1;
+//     } else if (crust === "stuffed") {
+//       return crustPrice.stuffed * 1;
+//     } else {
+//       return crustPrice.gluten * 1;
+//     }
+//   }
+//   // price according to topping
+
+//   function toppingsCalcPrice(toppings) {
+//     var noOfTopping = 0;
+//     for (i = 0; i < toppings.length; i++) {
+//       if (toppings[i] == "bacon") {
+//         noOfTopping += 150;
+//       }
+//       if (toppings[i] == "mushrooms") {
+//         noOfTopping += 100;
+//       }
+//       if (toppings[i] == "onions") {
+//         noOfTopping += 50;
+//       }
+//     }
+//     return noOfTopping * 1;
+//   }
+
+//   //function check for an element in array
+//   function checkBacon(topping) {
+//     return topping === "bacon";
+//   }
+  
+//   // *********UI Logic***********//
+//   $(document).ready(function() {
+    
+//       //fetch type of pizza
+//     function getPizzaType() {
+//         return $("#pizza-type")
+//           .find(":selected")
+//           .val();
+//       }
+//     //fetch size of pizza
+//     function getPizzaSize() {
+//       return $("#pizza-size")
+//         .find(":selected")
+//         .val();
+//     }
+//     //fetch crust of pizza
+//     function getCrust() {
+//       return $("#pizza-crust")
+//         .find(":selected")
+//         .val();
+//     }
+//     //fetch topping of pizza
+//     function getToppings() {
+//       var toppingList = [];
+//       $(".toppings :checked").each(function() {
+//         toppingList.push($(this).val());
+//       });
+//       return toppingList;
+//     }
+  
+//     //submit event
+//     $("form#myform").submit(function(event) {
+//       event.preventDefault();
+//       var pizzaType = getPizzaType();
+//       var pizzaSize = getPizzaSize();
+//       var crust = getCrust();
+//       var toppingList = getToppings();
+  
+//       var newPizza = new Pizza(pizzaType, pizzaSize, crust);
+//       newPizza.toppings.push(toppingList);
+//       $("#cart").hide();
+//       $("#table").show();
+//       $(".checkout").show();
+//       var oneOrder =
+//         sizeCalcPrice(pizzaSize) +
+//         crustCalcPrice(crust) +
+//         toppingsCalcPrice(toppingList);
+  
+//       //append item to the cart when submit event is triggered
+//       $("#items").append(
+//         "<tr>" +
+//           "<td>" +
+//           newPizza.type +
+//           "</td>" +
+//           "<td>" +
+//           "<p>" +
+//           newPizza.size +
+//           "</p>" +
+//           "</td>" +
+//           "<td>" +
+//           "<p>" +
+//           newPizza.crust +
+//           "</p>" +
+//           "</td>" +
+//           "<td>" +
+//           newPizza.toppings +
+//           "</td>" +
+//           "<td>" +
+//           oneOrder +
+//           "</td>" +
+//           "</tr>"
+//       );
+//     });
+//     var totalQuantity = parseInt($("#quantity").val());
+//     function calcTotal() {
+//       var priceOnePizza =
+//         sizeCalcPrice(getPizzaSize()) +
+//         crustCalcPrice(getCrust()) +
+//         toppingsCalcPrice(getToppings());
+//       return priceOnePizza;
+//     }
+//     var pizzaList = [];
+//     //what happens when submit button is triggered
+//     $("#orderbtn").on("click", function() {
+//       totalQuantity += 1;
+//       $("#quantity").text(totalQuantity);
+//       pizzaList.push(calcTotal());
+//     });
+  
+//     //display total prize of your order
+//     $("#gettotal").click(function() {
+//       var total = 0;
+//       pizzaList.forEach(function(pizza) {
+//         total += pizza;
+//       });
+//       $("#money").text(total);
+//     });
+  
+//     //event to trigger location form
+//     $("#myModel").click(function() {
+//       var deliver = confirm(
+//         "Would you like us deliver your pizza to you at a cost of ksh 250."
+//       );
+//       if (deliver) {
+//         var place = prompt(" Please enter your location");
+//         var finalPrice = calcTotal() * totalQuantity + 250;
+//         $("#place").text(place);
+//         $("#finalprice").text(finalPrice);
+//         $("#success").show();
+//       } else {
+//         $("#no-location").text(calcTotal() * totalQuantity);
+//         $("#no-delivery").show();
+//       }
+//       //clear 
+//       $("#pizza-type").val("");
+//       $("#pizza-size").val("");
+//       $("#pizza-crust").val("");
+//       $("#items").remove();
+//       $("#quantity").text(0);
+//     });
+//***********Busines Logic**********//
+
+//pizza constructor
+function Pizza(type, size, crust) {
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.toppings = [];
+  }
+  //price size
+  var sizePrice = {
+    small: 700,
+    medium: 900,
+    large: 1200
+  };
+  var toppingPrice = [
+    {
+      bacon: {
+        small: 50,
+        medium: 100,
+        large: 150
+      },
+      mushrooms: {
+        small: 50,
+        medium: 100,
+        large: 150
+      },
+      onions: {
+        small: 50,
+        medium: 100,
+        large: 150
+      }
+    }
+  ];
+  
+  function Location(name, estate) {
     this.name = name;
     this.estate = estate;
   }
+  
+  
+  
   //crust price
   var crustPrice = {
     crispy: 200,
     stuffed: 200,
     gluten: 150
   };
-  //function calculate prize according to size
+  //function calc prize according to size
   function sizeCalcPrice(size) {
     if (size === "small") {
       return sizePrice.small * 1;
@@ -70,39 +274,35 @@ function Location(name, estate) {
     }
   }
   // price according to topping
-
   function toppingsCalcPrice(toppings) {
     var noOfTopping = 0;
     for (i = 0; i < toppings.length; i++) {
       if (toppings[i] == "bacon") {
-        noOfTopping += 100;
+        noOfTopping += 150;
       }
       if (toppings[i] == "mushrooms") {
-        noOfTopping += 50;
+        noOfTopping += 100;
       }
       if (toppings[i] == "onions") {
-        noOfTopping += 75;
+        noOfTopping += 50;
       }
     }
     return noOfTopping * 1;
   }
   
-
   //function check for an element in array
   function checkBacon(topping) {
     return topping === "bacon";
   }
   
   // *********UI Logic***********//
-  $(document).ready(function() {
-    
-      //fetch type of pizza
+  $("document").ready(function() {
+    //fetch size of pizza
     function getPizzaType() {
         return $("#pizza-type")
           .find(":selected")
           .val();
       }
-    //fetch size of pizza
     function getPizzaSize() {
       return $("#pizza-size")
         .find(":selected")
@@ -143,27 +343,27 @@ function Location(name, estate) {
   
       //append item to the cart when submit event is triggered
       $("#items").append(
-        "<tr>" +
-          "<td>" +
-          newPizza.type +
-          "</td>" +
-          "<td>" +
-          "<p>" +
-          newPizza.size +
-          "</p>" +
-          "</td>" +
-          "<td>" +
-          "<p>" +
-          newPizza.crust +
-          "</p>" +
-          "</td>" +
-          "<td>" +
-          newPizza.toppings +
-          "</td>" +
-          "<td>" +
-          oneOrder +
-          "</td>" +
-          "</tr>"
+                "<tr>" +
+                  "<td>" +
+                  newPizza.type +
+                  "</td>" +
+                  "<td>" +
+                  "<p>" +
+                  newPizza.size +
+                  "</p>" +
+                  "</td>" +
+                  "<td>" +
+                  "<p>" +
+                  newPizza.crust +
+                  "</p>" +
+                  "</td>" +
+                  "<td>" +
+                  newPizza.toppings +
+                  "</td>" +
+                  "<td>" +
+                  oneOrder +
+                  "</td>" +
+                  "</tr>"
       );
     });
     var totalQuantity = parseInt($("#quantity").val());
@@ -194,11 +394,11 @@ function Location(name, estate) {
     //event to trigger location form
     $("#myModel").click(function() {
       var deliver = confirm(
-        "Would you like us deliver your pizza to you at a cost of ksh 250."
+        "Would you like us deliver your pizza to your doorstep? transport cost ksh 150."
       );
       if (deliver) {
-        var place = prompt(" Please enter your location");
-        var finalPrice = calcTotal() * totalQuantity + 250;
+        var place = prompt("Enter your location");
+        var finalPrice = calcTotal() * totalQuantity + 150;
         $("#place").text(place);
         $("#finalprice").text(finalPrice);
         $("#success").show();
@@ -227,6 +427,6 @@ function Location(name, estate) {
         );
         $("#contact")[0].reset();
       });
-      
   });
+   
   
